48091a1a400574915e54eb275d9a68908c31f265
JHONSILR7 <1007820221@unajma.edu.pe>
2025-12-05 07:15:39 -0500

    --

--- src/entornos/entorno.ts
+++ src/entornos/entorno.ts
@@ -1,6 +1,6 @@
 export const entorno = {
   produccion: false,
-  urlApi: 'http://laneria-mariano-backend-production.up.railway.app/api/v1',  // ← Agregar /v1
+  urlApi: 'https://laneria-mariano-backend-production.up.railway.app/api/v1',  // ← Agregar /v1
   nombreApp: 'Lanería Mariano Díaz',
   claveToken: 'token_autenticacion',
   claveUsuario: 'datos_usuario'
